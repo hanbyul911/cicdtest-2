6a741bd02ac0772d385e508d9c12392947da0490
hanbyul <unibyulse@gmail.com>
2024-11-26 16:25:26 +0900

    source commit

---
+++ src/main/java/com/example/cicdtest2/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.example.cicdtest2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+  @GetMapping("/")
+  public String main() {
+    return "This is cicdtest-02 자동배포";
+  }
+}
